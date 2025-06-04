f8834c205b961cade62244cab9880f6b9bc0d720
gerryvoy <gov_9@hotmail.com>
2025-06-04 16:48:41 -0600

    Add Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
+	adapter: cloudflare(),
 });
