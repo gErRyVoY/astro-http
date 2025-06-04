006f827ba974f5e8fb9944b6ee6114d9b826e160
gerryvoy <gov_9@hotmail.com>
2025-06-04 16:40:19 -0600

    First Commit

---
+++ src/pages/api/get-person.json.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from "astro";
+
+
+export const GET: APIRoute = async ({ params, request }) => {
+    const person = {
+        name: 'Gerry Voy',
+        age: 37,
+        email: 'gerardo.omana@humanitas.edu.mx'
+    };
+
+    return new Response(JSON.stringify(person), {
+        status: 201,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+};
