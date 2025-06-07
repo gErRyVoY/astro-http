e4c7469cea5928061ad76a3b870748b0b1ba6c10
gErRyVoY <gov_9@hotmail.com>
2025-06-07 14:42:26 -0600

    Turso added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,13 @@ import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 
 
+import db from '@astrojs/db';
+
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	output: 'server',
-	adapter: cloudflare(),
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), db()],
+    output: 'server',
+    adapter: cloudflare(),
 });
