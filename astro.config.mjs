2cbc025720defcfd3c6e904b068131582fbe328c
gErRyVoY <gov_9@hotmail.com>
2025-06-09 16:14:04 -0600

    Add netlify for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
-
-
+import netlify from '@astrojs/netlify';
 import db from '@astrojs/db';
 
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), db()],
-    output: 'server',
-    adapter: cloudflare(),
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), db()],
+  output: 'server',
+  adapter: netlify(),
 });
